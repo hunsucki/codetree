f83b9b2ee8207d3d409e33aa534688a6718f457c
Choi Hyeon Seok <50987401+hunsucki@users.noreply.github.com>
2026-08-09 10:42:03 +0900

    [Passed] Time to Time (2ms, 0MB)

---
+++ trail2/Time to Time/time-to-time.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+
+    cout << ((c * 60 + d) - (a * 60 + b)) << endl;
+    return 0;
+}
